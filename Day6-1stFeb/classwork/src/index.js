6b0704c92a670e3854b57db082178d3aece25a48
Manvendra Rajpoot <manvendra.rajpoot@ibm.com>
2022-02-07 09:00:52 +0530

    day-6 task

---
+++ Day6-1stFeb/classwork/src/index.js
@@ -0,0 +1,9 @@
+var parElem = document.getElementById("app");
+parElem.classList.add("newClass");
+
+//event listener
+var btn = document.getElementById("btn");
+btn.addEventListener("click", function () {
+  var newElem = document.createElement();
+});
+console.log("Button:- Click me>> ", btn);
